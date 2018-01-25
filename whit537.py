32f6f99e7d0247289642d348bbb767ac86b36452
Chad Whitacre <chad@zetaweb.com>
2018-01-25 17:48:14 -0500

    Dust off the homepage, burning off more crap

--- whit537.py
+++ whit537.py
@@ -1,48 +1,9 @@
-import datetime
 import os
-import re
 
-import tree
-from aspen import renderers
 from aspen.utils import Canonizer
 from aspen.website import Website
 
 
-class EvalRenderer(renderers.Renderer):
-    def render_content(self, context):
-        return eval(self.compiled, context)
-
-class EvalFactory(renderers.Factory):
-    Renderer = EvalRenderer
-
-
 website = Website([])
-website.renderer_default = 'unspecified'  # require explicit renderer, to avoid escaping bugs
-website.renderer_factories['eval'] = EvalFactory(website)
-website.default_renderers_by_media_type['text/html'] = 'jinja2'
-website.default_renderers_by_media_type['text/plain'] = 'jinja2'
-website.renderer_factories['jinja2'].Renderer.global_context = {
-    'enumerate': enumerate,
-}
-
-
 canonize = Canonizer(os.environ['CANONICAL_LOCATION'])
-
-
-def render_published(published):
-    if re.match('^.*-\d\d:\d\d$', published) is not None:
-        published = published[:-6]
-    out = datetime.datetime.strptime(published, '%Y-%m-%dT%H:%M:%S.%f')
-    out = out.strftime('%B %d, %Y')
-    out = out.replace(' 0', ' ')
-    return out
-
-
-def add_stuff(request):
-    return {'render_published': render_published}
-
-
-tree.startup(website)
-
 website.algorithm.insert_after('parse_environ_into_request', canonize)
-website.algorithm.insert_after('canonize', add_stuff)
